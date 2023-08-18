ae897eb7522c9964f72f38389fcac2404bc1cf51
jannoh <annohjoseph5@gmail.com>
2023-08-18 15:40:42 +0000

    Added math_functions.sh

---
+++ linuxTv_Bash/math_functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 00:00 - Intro
+# 01:00 - Math is handled differently in Bash
+# 01:28 - The "evaluate expression" command (expr) in Bash (used initially but switched to ((..)) expression)
+add=$((500 + 2))
+echo "Addition =: ${add}"
+
+sub=$((500 - 2))
+echo "Subtraction =: ${sub}"
+
+div=$((500 / 2))
+echo "Division =: ${div}"
+
+mod=$((500 % 2))
+echo "Modulus =: ${mod}"
+
+# 02:37 - Multiplication in Bash (and how to use an escape symbol)
+mul=$((500 * 2))
+echo "Multiplication =: ${mul}"
+
+# 03:48 - Using variables with math expressions
+length="100"
+breadth="7"
+echo "Area =: $((length * breadth))" # it is redundant to use $(${length})

fb62a8388a86f63de341ea685131530b5c288d14
jannoh <annohjoseph5@gmail.com>
2023-08-27 23:44:02 +0000

    Run basic linux commands

---
+++ RealLifeExamples_Scripting/run_basic_task.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Author: Joseph Annoh
+# Date Created: 27/08/2023
+# This script will run basic tasks and output  on the screen
+# Date Modified: 27/08/2023
+
+echo
+echo "Hello World"
+echo
+
+pwd
+echo
+ls
+echo
+whoami
+echo
+cal
+echo
+touch file1 file2 file3

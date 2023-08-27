765870f6bb548182217c36638ac0287beaa30c88
jannoh <annohjoseph5@gmail.com>
2023-08-27 23:22:48 +0000

    helloworld RLES

---
+++ RealLifeExamples_Scripting/hello_world.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Author: Joseph Annoh
+# Date Created: 27/08/2023
+# This script will output "Hello World" on the screen
+# Date Modified: 27/08/2023
+
+echo
+echo "Hello World"
+echo

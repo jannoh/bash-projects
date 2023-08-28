c509106b05262a89cca61b7151217927e8ac9eef
jannoh <annohjoseph5@gmail.com>
2023-08-28 14:47:22 +0000

    Run admin commands

---
+++ RealLifeExamples_Scripting/admin_task.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Author: Joseph Annoh
+# Date Created: 27/08/2023
+# Description: This scripts wil run basic admin commands
+# Date Modified: 27/08/2023
+echo
+echo "This script will run basic administrative commands"
+echo
+
+top
+echo
+df -h
+echo
+free -m
+echo
+uptime
+echo
+iostat
+echo
+echo "End of Script"

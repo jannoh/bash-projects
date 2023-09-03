b596c25b5794d4f6603c60cdd94a051ab0dfa9fa
jannoh <annohjoseph5@gmail.com>
2023-09-03 23:14:36 +0000

    Content count script

---
+++ RealLifeExamples_Scripting/scriptsDevops/content_count.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Author: Joseph Annoh
+# Date: 03/09/2023
+# Description: Get total number of files in a Directory
+# ./arguments.sh [directory_path] ...
+
+#check user arguments
+
+if [ $# -eq 0 ]; then
+    echo "This script requires exactly one or more directory path passed to it."
+    echo "Please try again."
+    exit 1
+elif [ $# -eq 1 ]; then
+    lines=$(ls -lh $1 | wc -l)
+    echo "You have $((lines - 1)) objects in the $1 directory"
+elif [ $# -gt 1 ]; then
+    echo $@
+    echo $#
+    for arg in "$@"; do
+        echo "$arg"
+        #    ls -lh "$arg"
+        ls -lh $arg &>/dev/null
+
+        if [ $? -eq 0 ]; then
+            lines=$(ls -lh $arg | wc -l)
+            echo "You have $((lines - 1)) objects in the $arg directory"
+        else
+            echo "Directory doesn't exist"
+        fi
+    done
+fi
